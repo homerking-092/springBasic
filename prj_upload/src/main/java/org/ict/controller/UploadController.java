c577a242924d03bc5468f65c9a1b642281159851
homerking-092 <dudgns3603@naver.com>
2021-10-14 19:01:03 +0900

    2021-10-14

--- prj_upload/src/main/java/org/ict/controller/UploadController.java
+++ prj_upload/src/main/java/org/ict/controller/UploadController.java
@@ -2,14 +2,27 @@ package org.ict.controller;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+
+import org.ict.domain.AttachFileDTO;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -81,10 +94,12 @@ public class UploadController {
 	// 일반 컨트롤러에서 rest요청을 처리시키는 경우에 @ResponseBody를 붙여줍니다
 	@ResponseBody
 	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 		
 		log.info("ajax post update!");
 		
+		// 그림 여러개를 받아야 하기 때문에 먼저 ArrayList를 생성
+		List<AttachFileDTO> list = new ArrayList<AttachFileDTO>();
 		String uploadFolder = "C:\\upload_data\\temp";
 		
 		// 폴더 생성
@@ -96,6 +111,12 @@ public class UploadController {
 		}
 		
 		for (MultipartFile multipartFile : uploadFile) {
+			
+			// 파일 이름, 폴더(업로드날짜), UUID, 그림파일 여부를 모두
+			// 이 반복문에서 처리하므로 제일 상단에 먼저 그림정보를 받는
+			// AttachFileDTO를 생성
+			AttachFileDTO attachDTO = new AttachFileDTO();
+			
 			log.info("----------------------");
 			log.info("Upload File Name: " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size: " + multipartFile.getSize());
@@ -108,20 +129,34 @@ public class UploadController {
 			
 			log.info("last file name: " + uploadFileName);
 			
+			// uploadFileName에 uuid가 포함되기 전에 세터에 넣어줘야
+			// 파일 이미지를 불러오는데 문제가 없음
+			attachDTO.setFileName(uploadFileName);
+			
 			// uuid 발급 부분
 			UUID uuid = UUID.randomUUID();
 			
 			uploadFileName = uuid.toString() + "_" + uploadFileName;
 			
-//			File saveFile = new File(uploadFolder, uploadFileName);
-			// 경로를 고정폴더인 uploadFolder에서 날짜별 가변폴더인 uploadPath로 변경
 			
 			try {
+//			File saveFile = new File(uploadFolder, uploadFileName);
+				// 경로를 고정폴더인 uploadFolder에서 날짜별 가변폴더인 uploadPath로 변경
 				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
 				
+				// AttachFileDTO에 세터로 데이터 입력
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(getFolder());
+				
 				// 이 아래부터 섬네일 생성로직
 				if (checkImageType(saveFile)) {
+					
+					// 클래스 생성 후 boolean타입은 자료입력을 하지 않으면
+					// 자동으로 false로 간주됨
+					// 이미지인 경우는 true를 넣고, 이미지가 아니면 그냥 건들지 않습니다
+					attachDTO.setImage(true);
+					
 					FileOutputStream thumbnail =
 							new FileOutputStream(
 									new File(uploadPath, "s_" + uploadFileName));
@@ -130,10 +165,99 @@ public class UploadController {
 							multipartFile.getInputStream(), thumbnail, 100, 100);
 					thumbnail.close();
 				}
+				
+				// 정상적으로 그림에 대한 정보가 입력되었다면 list에 쌓기
+				list.add(attachDTO);
+				
 			} catch (Exception e) {
 				log.error(e.getMessage());
 			}
 		}// end for
+		return new ResponseEntity<List<AttachFileDTO>>(list, HttpStatus.OK);
+		
 	}// end uploadAjaxPost()
+	
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName){
+		
+		log.info("fileName: " + fileName);
+		
+		File file = new File("c:\\upload_data\\temp\\" + fileName);
+		
+		log.info("file: " + file);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		try {
+			HttpHeaders header = new HttpHeaders();
+			
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			
+			result = new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+		
+	}// end getFile()
+	
+	@GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(String fileName){
+		log.info("download file: " + fileName);
+		
+		Resource resource = new FileSystemResource("C:\\upload_data\\temp\\" + fileName);
+		
+		log.info("resource: " + resource);
+		
+		String resourceName = resource.getFilename();
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		try {
+			headers.add("Content-Disposition", "attachment; filename=" +
+					new String(resourceName.getBytes("UTF-8"),
+												"ISO-8859-1"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	}// end downloadFile()
+	
+	@PostMapping("/deleteFile")
+	@ResponseBody
+	public ResponseEntity<String> deleteFile(String fileName, String type){
+		log.info("deleteFile: " + fileName);
+		
+		// 빈 파일, 추후에 삭제할 파일을 대입해 삭제 진행
+		File file = null;
+		
+		try {
+			// url의 파일 이름을 원래대로 복원시킴
+			file = new File("c:\\upload_data\\temp" + URLDecoder.decode(fileName, "UTF-8"));
+			
+			file.delete();
+			
+			// 웹화면에서 썸네일을 먼저 삭제하기 떄문에 원본파일도 마저 잡아서 삭제해야함
+			if (type.equals("image")) {
+				
+				// 썸네일 파일명에서 s_를 소거해 원본으로 교체
+				String largeFileName = file.getAbsolutePath().replace("s_", "");
+				
+				log.info("원본 파일명: " + largeFileName);
+				
+				file = new File(largeFileName);
+				
+				file.delete();
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<String>("deleted", HttpStatus.OK);
+	}// end deleteFile()
 
 }// end class
--- prj_upload/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ prj_upload/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -3,6 +3,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style type="text/css">
+	.uploadResult{
+		width: 100%;
+		background-color: gray;
+	}
+	.uploadResult ul{
+		display: flex;
+		flex-flow: row;
+		justify-content: center;
+		align-items: center;
+	}
+	.uploadResult ul li{
+		list-style: none;		
+		padding: 10px;
+	}
+	.uploadResult ul li img{
+		width: 20px;
+	}
+</style>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
@@ -12,6 +31,12 @@
 
 	<div class="uploadDiv">
 		<input type="file" name="uploadFile" multiple>
+	</div>
+	
+	<div class="uploadResult">
+		<ul>
+			<!-- 업로드된 파일이 들어갈 자리 -->
+		</ul>
 	</div>
 
 	<button id="uploadBtn">Upload</button>
@@ -39,6 +64,10 @@
 				return true;
 			}
 
+			// 업로드시 파일 선택을 초기화시키기 위해 미리 업로드되지 않은
+			// .uploadDiv를 깊은복사로 복사해둠
+			var cloneObj = $(".uploadDiv").clone();
+			
 			$('#uploadBtn').on("click", function(e) {
 
 				// ajax는 제출버튼을 눌렀을때 목적지가 없기 때문에
@@ -60,6 +89,7 @@
 					}
 					formData.append("uploadFile", files[i]);
 				}
+				console.log(formData);
 
 				// 비동기요청
 				$.ajax({
@@ -68,13 +98,94 @@
 					contentType : false,
 					data : formData,
 					type : 'POST',
+					dataType: 'json',	// 입력시 json으로 콘솔에, 안입력하면 xml로 콘솔에 찍힘
 					success : function(result) {
-						alert("uploaded");
+						console.log(result);	// 내가 업로드한 파일 내역이 콘솔에 찍히나 디버깅
+						// alert("uploaded");
+						
+						// 업로드된 그림파일 목록을 ul내부에 리스트로 입력
+						showUploadedFile(result);
+						
+						// 세팅되어있던 파일이 업로드되면서 목록에서 사라지게 처리
+						$(".uploadDiv").html(cloneObj.html());
 					}
 				});// $.ajax
-
-			});
-		});
+			});	// onclick uploadBtn
+			
+			var uploadResult = $(".uploadResult ul");
+			
+			function showUploadedFile(uploadResultArr) {
+				var str = "";
+				
+				// i는 인덱스번호(0, 1, 2....) obj 그림파일 정보가 담긴 json
+				$(uploadResultArr).each(function (i, obj) {
+					
+					console.log("------------------");
+					console.log(i);
+					console.log(obj);
+					console.log("-------------------");
+					
+					if (!obj.image) {
+						
+						var fileCallPath = encodeURIComponent(
+							obj.uploadPath + "/"
+						   + obj.uuid + "_" +obj.fileName);
+						
+						// 그림이 아니면 썸네일대신 resources폴더 내 이미지를 대체로 표시
+						str += "<li><a href='/download?fileName=" + fileCallPath
+							+"'><img src = '/resources/attach.png'>"	/// 이미지파일 경로 고정
+							+ obj.fileName + "</a>"
+							+ "<span data-file=\'" + fileCallPath + "\' data-type='file'> X </span>"
+							+ "</li>";
+					} else {
+						// str += "<li>" + obj.fileName + "</li>";
+						// 파일이름 + 썸네일을 보여주기 위해 썸네일 주소 요청하게 만들기
+						
+						// 수정코드
+						/// 섬네일 다운
+						var fileCallPath = encodeURIComponent(obj.uploadPath
+								+ "/s_" + obj.uuid + "_" + obj.fileName);
+						/// 원본 다운
+						var fileCallPath2 = encodeURIComponent(obj.uploadPath
+								+ "/" + obj.uuid + "_" + obj.fileName);
+						
+						// 원래 그냥 fileCallPath를 조립해서
+						str += "<li><a href='/download?fileName=" + fileCallPath2
+							+"'><img src= '/display?fileName="
+							+fileCallPath +"'> " + obj.fileName + "</a>"
+							+ "<span data-file=\'" + fileCallPath + "\' data-type='image'> X </span>"
+							+ "</li>";
+					}
+					
+				});
+				
+				uploadResult.append(str);
+			}// showUploadedFile
+			
+			$(".uploadResult").on("click", "span", function (e) {
+				
+				console.log($(e));
+				
+				console.log($(this));				
+				var targetFile = $(this).data("file");
+				var type = $(this).data("type");
+				console.log(targetFile + "///" + type);
+				
+				var targetLi = $(this).closest("li");
+				console.log(targetLi);
+				$.ajax({
+					url : '/deleteFile',
+					data : {fileName : targetFile, type:type},
+					dataType : 'text',
+					type : 'POST',
+					success : function(result){
+						alert(result);
+						targetLi.remove();
+					}
+					
+				});	//end $.ajax
+			});	// end click span
+		});// end document
 	</script>
 </body>
 </html>
